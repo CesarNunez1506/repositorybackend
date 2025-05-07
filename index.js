7dc9bb2603a52779812e9f5ad4fdc961d54f7cd1
CesarNunez1506 <cesar.nunez@tecsup.edu.pe>
2025-05-07 14:05:48 -0500

    Cambios index

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 require('dotenv').config()
 const http= require('http')
 
-function requestController(){
+function requestController(req, res){
     console.log('Bienvenido al curso')
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hola desde el servidor Node.js');
 }
+
 
 const server=http.createServer(requestController)
 
